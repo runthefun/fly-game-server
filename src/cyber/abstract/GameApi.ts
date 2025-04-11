2c2ff02ed4399db9af8e01fb36e294792d878ed6
Yassine EL OUAFI <yelouafi@gmail.com>
2025-04-11 17:08:25 +0100

    load from vibe

--- src/cyber/abstract/GameApi.ts
+++ src/cyber/abstract/GameApi.ts
@@ -1,6 +1,9 @@
-const BASE_URL = "https://oo-git-dev-oncyber.vercel.app";
-// const BASE_URL = "http://localhost:3000";
-const GAME_API_URL = `${BASE_URL}/api/games`;
+//const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://v3-oncyber.vercel.app";
+const GAME_API_URL = `${BASE_URL}/api/games/awe`;
+
+const AWE_BASE_URL = "https://oo-git-dev-oncyber.vercel.app";
+const AWE_GAME_API_URL = `${AWE_BASE_URL}/api/games`;
 
 const GAME_SERVER_KEY = process.env.GAME_SERVER_KEY;
 
@@ -9,9 +12,81 @@ delete process.env.GAME_SERVER_KEY;
 export class GameApi {
   //
   static async loadGameData(opts: { id: string; draft?: boolean }) {
+    // load concurrently from both endpoints and whoever returns first and not error wins
+    return new Promise((resolve, reject) => {
+      //
+      let results = {
+        awe: undefined,
+        vibe: undefined,
+      };
+      let settled = false;
+
+      const trySettle = () => {
+        if (settled) return;
+        // if both are null, we need to reject
+        if (results.awe === null && results.vibe === null) {
+          reject(new Error("failed to load game data"));
+        }
+        // one of them is resolved
+        Object.keys(results).forEach((key) => {
+          const value = results[key];
+          if (value) {
+            settled = true;
+            resolve(value);
+          }
+        });
+      };
+
+      GameApi.loadGameDataAwe(opts).then(
+        (aweResult) => {
+          results.awe = aweResult;
+          trySettle();
+        },
+        (err) => {
+          // console.error(err);
+          results.awe = null;
+          trySettle();
+        }
+      );
+
+      GameApi.loadGameDataVibe(opts).then(
+        (vibeResult) => {
+          //
+          results.vibe = vibeResult;
+          trySettle();
+        },
+        (err) => {
+          // console.error(err);
+          results.vibe = null;
+          trySettle();
+        }
+      );
+    });
+  }
+
+  static async loadGameDataVibe(opts: { id: string; draft?: boolean }) {
+    // fetch the space here...
+    const url = `${GAME_API_URL}?gameId=${opts.id}&draft=${opts.draft}`;
+    // console.log("url", url);
+    const reponse = await fetch(url, {
+      headers: {
+        "x-server-key": GAME_SERVER_KEY,
+      },
+    });
+
+    if (!reponse.ok) {
+      throw new Error("failed to load game data (vibe)");
+    }
+
+    const result = await reponse.json();
+
+    return result;
+  }
+
+  static async loadGameDataAwe(opts: { id: string; draft?: boolean }) {
     // fetch the space here...
     const reponse = await fetch(
-      `${GAME_API_URL}/${opts.id}?draft=${opts.draft}`,
+      `${AWE_GAME_API_URL}/${opts.id}?draft=${opts.draft}`,
       {
         headers: {
           "x-server-key": GAME_SERVER_KEY,
